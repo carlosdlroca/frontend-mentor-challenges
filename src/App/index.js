b724eb632a19be727600ebfaed7744e837b948d5
Carlos De La Roca <carlosdlroca80@gmail.com>
2020-04-20 12:00:37 -0700

    Remove useState

--- src/App/index.js
+++ src/App/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Router from "./Router";
 import { ThemeProvider } from "styled-components";
 import BaseStyles from "./Styles/BaseStyles";
