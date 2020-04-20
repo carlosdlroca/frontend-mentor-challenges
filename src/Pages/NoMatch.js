7858616593e5449346dc7ce8e5b3400fe8c712ba
Carlos De La Roca <carlosdlroca80@gmail.com>
2020-04-20 12:01:11 -0700

    Create NoMatch page

---
+++ src/Pages/NoMatch.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Navbar from "../components/Navbar";
+
+export default ({ toggleTheme }) => (
+    <React.Fragment>
+        <Navbar toggleTheme={toggleTheme} />
+        <h1>Sorry, that page doesnt exist</h1>
+        <ul style={{ display: "flex", flexDirection: "column" }}>
+            <li>
+                <Link to='/'>Home</Link>
+            </li>
+            <li>
+                <Link to='/challenges'>Challenges</Link>
+            </li>
+        </ul>
+    </React.Fragment>
+);
