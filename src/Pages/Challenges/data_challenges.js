8d18e25cd93fae92a776834ed9309b8b470dbf96
Carlos De La Roca <carlosdlroca80@gmail.com>
2020-04-20 19:47:02 -0700

    Create store for challenges

---
+++ src/Pages/Challenges/data_challenges.js
@@ -0,0 +1,32 @@
+import FourCard from "./Newbie/FourCard";
+import ComingSoon from "./Newbie/ComingSoon";
+import SignUpForm from "./Newbie/SignUpForm";
+
+export default {
+    newbie: [
+        {
+            name: "Four Card Feature Section",
+            path: "/challenges/newbie/four-card-feature-section",
+            image: "/challenges/newbie/four-card-feature.jpg",
+            frontend_mentor_link:
+                "https://www.frontendmentor.io/challenges/four-card-feature-section-weK1eFYK",
+            component: FourCard,
+        },
+        {
+            name: "Base Apparel Coming Soon Page",
+            path: "/challenges/newbie/base-apparel-coming-soon-page",
+            image: "/challenges/newbie/coming-soon-page.jpg",
+            frontend_mentor_link:
+                "https://www.frontendmentor.io/challenges/base-apparel-coming-soon-page-5d46b47f8db8a7063f9331a0",
+            component: ComingSoon,
+        },
+        {
+            name: "Intro Component with sign-up form",
+            path: "/challenges/newbie/intro-component-with-sign-up-form",
+            image: "/challenges/newbie/intro-component.jpg",
+            frontend_mentor_link:
+                "https://www.frontendmentor.io/challenges/intro-component-with-signup-form-5cf91bd49edda32581d28fd1",
+            component: SignUpForm,
+        },
+    ],
+};
