9a1b2b81026bb328d5679c055455dd109d7774c8
Carlos De La Roca <carlosdlroca80@gmail.com>
2020-04-20 19:47:36 -0700

    Add Card

--- src/Pages/Challenges/ChallengesList.js
+++ src/Pages/Challenges/ChallengesList.js
@@ -1,9 +1,26 @@
 import React from "react";
+import { ChallengesListPage } from "./Styles";
+import Grid from "../../components/Grid";
+import Card from "../../components/Card";
 
-import { useParams } from "react-router-dom";
-
-export default () => {
-    const { difficulty } = useParams();
-
-    return <h1>{difficulty} challenges are a work in progress</h1>;
+export default ({ challenges }) => {
+    const { newbie } = challenges;
+    const renderChallengeCard = ({
+        name,
+        path,
+        image,
+        frontend_mentor_link,
+    }) => (
+        <Card key={name} title={name} image_src={image} link_url={path}>
+            <a style={{ fontSize: "1.8rem" }} href={frontend_mentor_link}>
+                visit original challenge
+            </a>
+        </Card>
+    );
+    return (
+        <ChallengesListPage>
+            <h1>newbie Challenges: </h1>
+            <Grid>{newbie.map(renderChallengeCard)}</Grid>
+        </ChallengesListPage>
+    );
 };
