c7e09d55bd465657188a79219ff0234eb9770d56
Carlos De La Roca <carlosdlroca80@gmail.com>
2020-04-27 12:40:40 -0700

    Change theme colors

--- src/App/Styles/themes.js
+++ src/App/Styles/themes.js
@@ -1,14 +1,14 @@
 export const lightTheme = {
-    main: "#990011",
-    sub: "#ff4949",
+    main: "#7971EA",
+    sub: "#A69EFC",
     tertiary: "#12d3cf",
     text: "#424874",
     body: "#FFF",
 };
 
 export const darkTheme = {
-    main: "#990011",
-    sub: "#ff4949",
+    main: "#7971EA",
+    sub: "#A69EFC",
     tertiary: "#12d3cf",
     text: "white",
     body: "#424874",
