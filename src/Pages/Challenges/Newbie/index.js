a5e0d4646def73239411697857096f7de5f9bc89
Carlos De La Roca <carlosdlroca80@gmail.com>
2020-05-06 12:13:12 -0700

    Add Lazy Loading

--- src/Pages/Challenges/Newbie/index.js
+++ src/Pages/Challenges/Newbie/index.js
@@ -1,6 +1,8 @@
-import ComingSoon from "./ComingSoon";
-import FourCards from "./FourCards";
-import SignUpForm from "./SignUpForm";
-import SinglePrice from "./SinglePrice";
+import loadable from "@loadable/component";
+
+const ComingSoon = loadable(() => import("./ComingSoon"));
+const FourCards = loadable(() => import("./FourCards"));
+const SignUpForm = loadable(() => import("./SignUpForm"));
+const SinglePrice = loadable(() => import("./SinglePrice"));
 
 export default { ComingSoon, FourCards, SignUpForm, SinglePrice };
