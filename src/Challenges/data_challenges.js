ea18d054c32c1f2b16fb1c0b7248adae05fc298b
Carlos De La Roca <carlosdlroca80@gmail.com>
2020-05-07 09:26:57 -0700

    capitalize all 'newbie' words

--- src/Challenges/data_challenges.js
+++ src/Challenges/data_challenges.js
@@ -5,8 +5,8 @@ export default {
         {
             name: "Four Card Feature Section",
             param: "FourCards",
-            path: "/challenges/newbie/FourCards",
-            image: "/challenges/newbie/four-card-feature.jpg",
+            path: "/challenges/Newbie/FourCards",
+            image: "/challenges/Newbie/four-card-feature.jpg",
             frontend_mentor_link:
                 "https://www.frontendmentor.io/challenges/four-card-feature-section-weK1eFYK",
             component: Newbie.FourCards,
@@ -14,8 +14,8 @@ export default {
         {
             name: "Base Apparel Coming Soon Page",
             param: "ComingSoon",
-            path: "/challenges/newbie/ComingSoon",
-            image: "/challenges/newbie/coming-soon-page.jpg",
+            path: "/challenges/Newbie/ComingSoon",
+            image: "/challenges/Newbie/coming-soon-page.jpg",
             frontend_mentor_link:
                 "https://www.frontendmentor.io/challenges/base-apparel-coming-soon-page-5d46b47f8db8a7063f9331a0",
             component: Newbie.ComingSoon,
@@ -23,8 +23,8 @@ export default {
         {
             name: "Intro Component with sign-up form",
             param: "SignUpForm",
-            path: "/challenges/newbie/SignUpForm",
-            image: "/challenges/newbie/intro-component.jpg",
+            path: "/challenges/Newbie/SignUpForm",
+            image: "/challenges/Newbie/intro-component.jpg",
             frontend_mentor_link:
                 "https://www.frontendmentor.io/challenges/intro-component-with-signup-form-5cf91bd49edda32581d28fd1",
             component: Newbie.SignUpForm,
@@ -32,8 +32,8 @@ export default {
         {
             name: "Single Price Grid Component",
             param: "SinglePrice",
-            path: "/challenges/newbie/SinglePrice",
-            image: "/challenges/newbie/single-price-grid-component.jpg",
+            path: "/challenges/Newbie/SinglePrice",
+            image: "/challenges/Newbie/single-price-grid-component.jpg",
             frontend_mentor_link:
                 "https://www.frontendmentor.io/challenges/single-price-grid-component-5ce41129d0ff452fec5abbbc",
             component: Newbie.SinglePrice,
@@ -41,8 +41,8 @@ export default {
         {
             name: "Launching Soon",
             param: "LaunchingSoon",
-            path: "/challenges/newbie/LaunchingSoon",
-            image: "/challenges/newbie/launching-soon.jpg",
+            path: "/challenges/Newbie/LaunchingSoon",
+            image: "/challenges/Newbie/launching-soon.jpg",
             frontend_mentor_link:
                 "https://www.frontendmentor.io/challenges/ping-single-column-coming-soon-page-5cadd051fec04111f7b848da",
             component: Newbie.LaunchingSoon,
