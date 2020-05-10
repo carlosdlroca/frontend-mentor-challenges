b6a0605857dd53de7deeb79c151728d15f3a6220
Carlos De La Roca <carlosdlroca80@gmail.com>
2020-05-10 12:52:26 -0700

    Complete Challenge

--- src/Challenges/Newbie/LaunchingSoon/Styles.js
+++ src/Challenges/Newbie/LaunchingSoon/Styles.js
@@ -14,20 +14,175 @@ export const Page = styled.main`
     display: flex;
     flex-direction: column;
     align-items: center;
+    justify-content: flex-end;
 
-    padding: 10rem 1rem;
+    padding: 6rem 1rem 10rem;
+
+    img {
+        max-width: 50%;
+
+        @media only screen and (max-width: 1024px) {
+            max-width: 75%;
+        }
+    }
 `;
 
 export const Heading = styled.header`
-    font-weight: 300;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 4rem 0;
     .Title {
         font-size: 4rem;
+        font-weight: 100;
         .soon {
             font-weight: 600;
             color: var(--color-darkblue);
         }
     }
     .SubTitle {
-        font-size: 1.5rem;
+        margin: 1rem 0;
+        color: var(--color-darkblue);
+        font-size: 2rem;
+        font-weight: 400;
+    }
+
+    @media only screen and (max-width: 1024px) {
+        .Title {
+            font-size: 3rem;
+        }
+        .SubTitle {
+            font-size: 1.5rem;
+        }
+    }
+`;
+
+export const Form = styled.form`
+    display: flex;
+    justify-content: space-between;
+    width: 85%;
+    position: relative;
+    margin-bottom: 4rem;
+    /* flex-flow: row wrap; */
+    input {
+        font-size: 1.4rem;
+        flex: 0 0 65%;
+        border-radius: 4rem;
+        border: 1px solid var(--color-paleblue);
+        padding-left: 1rem;
+
+        &:invalid {
+            border-color: var(--color-lightred);
+
+            & + .ErrorMessage {
+                bottom: -1.5rem;
+                opacity: 1;
+            }
+        }
+    }
+
+    button {
+        flex: 0 0 35%;
+        margin-left: 2rem;
+        font-size: 1.7rem;
+        border-radius: 4rem;
+        background: var(--color-blue);
+        padding: 1.5rem 4.5rem;
+        box-shadow: 0 0 1rem var(--color-blue);
+        color: white;
+
+        &:hover {
+            filter: brightness(1.2);
+        }
+    }
+
+    .ErrorMessage {
+        opacity: 0;
+        position: absolute;
+        bottom: 100%;
+        left: 0;
+        color: var(--color-lightred);
+        font-size: 1.2rem;
+        user-select: none;
+        transition: all 0.2s linear;
+    }
+
+    @media only screen and (max-width: 1024px) {
+        flex-flow: row wrap;
+        input,
+        button {
+            flex: 0 0 100%;
+        }
+
+        button {
+            margin-left: 0;
+            margin-bottom: 2rem;
+        }
+        input {
+            padding: 1.5rem 1rem;
+            margin-bottom: 5rem;
+
+            &:invalid {
+                & + .ErrorMessage {
+                    bottom: calc(100% - 6.2rem);
+                    opacity: 1;
+                }
+            }
+        }
+    }
+
+    @media only screen and (max-width: 565px) {
+        width: 100%;
+        button {
+            margin-left: 0.3rem;
+            font-size: 1.2rem;
+        }
+    }
+
+    @media only screen and (min-width: 1024px) {
+        width: 40%;
+    }
+`;
+
+export const Footer = styled.footer`
+    color: var(--color-gray);
+    font-size: 1.4rem;
+    ul {
+        display: flex;
+        justify-content: space-evenly;
+        margin-bottom: 1.5rem;
+        .Link {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            border: 1px solid var(--color-blue);
+            border-radius: 50%;
+            transition: all 0.2s ease;
+            width: 4rem;
+            height: 4rem;
+
+            svg {
+                width: 2.5rem;
+                height: 2.5rem;
+                transition: fill 0.2s ease;
+                fill: var(--color-blue);
+            }
+
+            &:hover {
+                background: var(--color-blue);
+                cursor: pointer;
+                svg {
+                    fill: white;
+                }
+            }
+
+            &:not(:last-child) {
+                margin-right: 1rem;
+            }
+        }
+    }
+
+    @media only screen and (max-width: 1024px) {
+        margin-top: 4rem;
     }
 `;
--- src/Challenges/Newbie/LaunchingSoon/index.js
+++ src/Challenges/Newbie/LaunchingSoon/index.js
@@ -1,6 +1,11 @@
 import React from "react";
-import { Page, Heading } from "./Styles";
+import { Page, Heading, Form as StyledForm, Footer } from "./Styles";
 import { ReactComponent as Logo } from "./media/logo.svg";
+import Dashboard from "./media/illustration-dashboard.png";
+
+import { ReactComponent as Facebook } from "../../shared/icons/facebook-f.svg";
+import { ReactComponent as Twitter } from "../../shared/icons/twitter.svg";
+import { ReactComponent as Instagram } from "../../shared/icons/instagram.svg";
 
 export default () => {
     return (
@@ -12,6 +17,34 @@ export default () => {
                 </h1>
                 <h2 className='SubTitle'>Subscribe and get notified</h2>
             </Heading>
+            <Form />
+            <img src={Dashboard} alt={"dashboard"} />
+            <Footer>
+                <ul>
+                    <li className='Link'>
+                        <Facebook />
+                    </li>
+                    <li className='Link'>
+                        <Twitter />
+                    </li>
+                    <li className='Link'>
+                        <Instagram />
+                    </li>
+                </ul>
+                &copy; Copyright Ping. All rights reserved.
+            </Footer>
         </Page>
     );
 };
+
+function Form() {
+    return (
+        <StyledForm onSubmit={(e) => e.preventDefault()}>
+            <input placeholder='Your Email Address' type='email' />
+            <span className='ErrorMessage'>
+                Please provide a valid email address
+            </span>
+            <button>Notify Me</button>
+        </StyledForm>
+    );
+}
