2af20d9adebc50f17941036ef907f3e5afceaf54
Carlos De La Roca <carlosdlroca80@gmail.com>
2020-05-27 12:51:35 -0700

    Add style to difficulty title

--- src/Challenges/Styles.js
+++ src/Challenges/Styles.js
@@ -1,3 +1,11 @@
 import styled from "styled-components";
 
-export const ChallengesListPage = styled.main``;
+export const ChallengesListPage = styled.main`
+    h1 {
+        text-align: center;
+        font-size: 5rem;
+        text-decoration: underline;
+        text-decoration-color: var(--color-main);
+        text-decoration-style: wavy;
+    }
+`;
