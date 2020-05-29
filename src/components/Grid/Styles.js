f71e4a07f7f4baeeddaf1c33e152a04dd27e6382
Carlos De La Roca <carlosdlroca80@gmail.com>
2020-05-28 19:27:04 -0700

    Update media query

--- src/components/Grid/Styles.js
+++ src/components/Grid/Styles.js
@@ -4,11 +4,10 @@ export default styled.ul`
     display: grid;
     grid-template-columns: repeat(auto-fill, minmax(40rem, 1fr));
     grid-gap: 2rem;
-    /* background: red; */
     width: 80%;
     margin: 4rem auto;
 
-    @media only screen and (max-width: 768px) {
+    @media only screen and (max-width: 1380px) {
         width: 97%;
     }
 `;
